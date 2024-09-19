77d8cae26b4496956009be3dc5ebb6b5cba26186
hitsz <hitsz@hitsz.com>
2024-09-19 10:19:23 +0800

    update

---
+++ src/cn/edu/hitsz/compiler/lexer/LexerState.java
@@ -0,0 +1,10 @@
+package cn.edu.hitsz.compiler.lexer;
+
+public enum LexerState {
+    START,
+    LETTER,
+    DIGIT,
+//    MULTI,
+//    QUOTE,
+    EQ
+}
--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -4,7 +4,13 @@ import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
+import java.util.logging.Logger;
 
 /**
  * TODO: 实验一: 实现词法分析
@@ -15,6 +21,10 @@ import java.util.stream.StreamSupport;
  * @see TokenKind 词法单元类型的实现
  */
 public class LexicalAnalyzer {
+    private static final Logger logger = Logger.getLogger(LexicalAnalyzer.class.getName());
+    List<Token> tokens = new ArrayList<>();
+    StringBuilder contends = new StringBuilder();
+
     public LexicalAnalyzer(SymbolTable symbolTable) {
     }
 
@@ -28,7 +38,23 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        List<String> buffer = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                buffer.add(line);
+            }
+        } catch (IOException e) {
+            logger.severe("loadFile Failed: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        // 你可以在这里处理缓冲区数据
+        // 例如，打印或进行词法分析
+        for (String line : buffer) {
+            contends.append(line).append('\n');
+        }
     }
 
     /**
@@ -36,9 +62,46 @@ public class LexicalAnalyzer {
      * 需要维护实验一所需的符号表条目, 而得在语法分析中才能确定的符号表条目的成员可以先设置为 null
      */
     public void run() {
-        // TODO: 自动机实现的词法分析过程
-        throw new NotImplementedException();
+        LexerState state = LexerState.START;
+        tokens = new ArrayList<>();
+        StringBuilder tmp = new StringBuilder();
+        for (char c : contends.toString().toCharArray()) {
+            switch (state) {
+                case START -> {
+                    if (Character.isWhitespace(c)) {
+                        state = LexerState.START;
+                    } else if (Character.isLetter(c)) {
+                        state = LexerState.LETTER;
+                    } else if (Character.isDigit(c)) {
+                        state = LexerState.DIGIT;
+                    } else if (c == '*') {
+                        state = LexerState.MULTI;
+                    } else if (c == '=') {
+                        state = LexerState.EQ;
+//                    } else if (c == '\"') {
+//                        state = LexerState.QUOTE;
+                    } else if (c == '(') {
+
+                    } else if (c == ')') {
+
+                    } else if (c == ':') {
+
+                    } else if (c == '+') {
+
+                    } else if (c == '-') {
+
+                    } else if (c == '/') {
+
+                    } else if (c == ',') {
+
+                    } else if (c == ';') {
+
+                    }
+                }
+            }
+        }
     }
+
 
     /**
      * 获得词法分析的结果, 保证在调用了 run 方法之后调用
@@ -50,15 +113,14 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
-        throw new NotImplementedException();
+
+        return null;
     }
 
     public void dumpTokens(String path) {
-        FileUtils.writeLines(
-            path,
-            StreamSupport.stream(getTokens().spliterator(), false).map(Token::toString).toList()
-        );
+        FileUtils.writeLines(path, StreamSupport.stream(getTokens().spliterator(), false).map(Token::toString).toList());
     }
 
 
 }
+
